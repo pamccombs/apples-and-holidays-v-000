cea0d4c601784dc8e24324046b99dc87eeccf432
Patrick McCombs <pa.mccombs1@gmail.com>
2018-07-22 21:21:16 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,10 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+holiday_hash.each do |season, holiday|
+  holiday.each do |holiday, symbols|
+end
+end
 end
 
 def all_holidays_with_bbq(holiday_hash)
